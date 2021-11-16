5db25f9b367a97250202edcb6f2cac820c65b366
ogubuike Alex <ogubuikealex@gmail.com>
2021-11-16 18:00:07 +0100

    Updated ReturnModel, Added enumToList converter, Implemented random walletId generator

---
+++ KingsWalletAPI.Model/Helpers/Converter.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using KingsWalletAPI.Model.Entites;
+
+namespace KingsWalletAPI.Model.Helpers
+{
+    public static class Converter
+    {
+        public static IEnumerable ConvertEnumToList(Enum model)
+        {
+            var agencyNames = Enum.GetNames(model.GetType()); 
+
+            return agencyNames.ToList();
+        }
+    }
+}
---
+++ KingsWalletAPI.Model/Helpers/ReturnModel.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KingsWalletAPI.Model.Helpers
+{
+    public class ReturnModel
+    {
+        public ReturnModel()
+        {
+
+        }
+
+        public ReturnModel(bool success, string message)
+        {
+            Success = success;
+
+            Message = message;
+        }
+
+        public ReturnModel(bool success, string message, object _object) : this (success, message)
+        {
+            Object = _object;
+        }
+
+        public bool Success { get; set; }
+
+        public string Message { get; set; }
+
+        public object Object { get; set; }
+
+    }
+}
